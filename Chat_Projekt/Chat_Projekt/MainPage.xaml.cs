8a26a5c0fce9fe05247b5e4af5986094395effcd
Justin Scheel <scheel@th-brandenburg.de>
2019-05-28 22:44:38 +0200

    added all existing project files!

---
+++ Chat_Projekt/Chat_Projekt/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chat_Projekt
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        static IPAddress ipAddress = IPAddress.Parse("192.168.2.106");
+        static IPEndPoint remoteEP = new IPEndPoint(ipAddress, 1600);
+        byte[] sendMsg = new byte[4096];
+        byte[] receivedMsg = new byte[4096];
+
+        static Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        public MainPage()
+        {
+            InitializeComponent();
+            client.Connect(remoteEP);
+            Ausgabe.Text = "Socket connected to " + client.RemoteEndPoint.ToString();
+        }
+
+        private void SendenButton_Clicked(object sender, EventArgs e)
+        {
+
+            sendMsg = Encoding.ASCII.GetBytes(Eingabe.Text);
+            
+            client.Send(sendMsg);
+            client.Receive(receivedMsg);
+            Ausgabe.Text = Encoding.ASCII.GetString(receivedMsg);
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace Server
+{
+    class Program
+    {
+        public static bool SocketConnected(Socket s)
+        {
+            bool part1 = s.Poll(1000, SelectMode.SelectRead);
+            bool part2 = (s.Available == 0);
+            if (part1 && part2)
+                return false;
+            else
+                return true;
+        }
+        static void Main(string[] args)
+        {
+            byte[] sendMsg = new byte[4096];
+            sendMsg = Encoding.ASCII.GetBytes("Nachricht erhalten!");
+            byte[] receivedMsg = new byte[4096];
+            String myIp = "";
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+            
+
+            foreach (IPAddress ip in ipHostInfo.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip;
+                    myIp = ip.ToString();
+                }
+            }
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 1600);
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
+            listener.Bind(localEndPoint);
+
+            listener.Listen(10);
+
+            Console.WriteLine("Waiting for a connection...");
+            
+            Socket handler = listener.Accept();
+            while (true)
+            {
+
+                if (SocketConnected(handler))
+                {
+                    Console.WriteLine("verbunden");
+                    handler.Receive(receivedMsg);
+
+                    
+                    int i = receivedMsg.Length - 1;
+                    while (receivedMsg[i] == 0)
+                        --i;
+
+                    byte[] bar = new byte[i + 1];
+                    for (int x = 0; x < i + 1; x++)
+                    {
+                        bar[x] = receivedMsg[x];
+                    }
+
+                    Console.WriteLine(Encoding.ASCII.GetString(bar));
+                    handler.Send(sendMsg);
+                }
+                
+            }
+        }
+    }
+}
