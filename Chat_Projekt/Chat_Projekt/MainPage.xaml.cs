f386f3767acda9882ad67815c173959454021301
Justin Scheel <scheel@th-brandenburg.de>
2019-06-10 17:14:16 +0200

    Client changes

--- Chat_Projekt/Chat_Projekt/MainPage.xaml.cs
+++ Chat_Projekt/Chat_Projekt/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 
@@ -15,27 +16,64 @@ namespace Chat_Projekt
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        static IPAddress ipAddress = IPAddress.Parse("192.168.0.5");
+        static IPAddress ipAddress = IPAddress.Parse("192.168.2.107"); //84.173.244.81    192.168.2.107
         static IPEndPoint remoteEP = new IPEndPoint(ipAddress, 1600);
-        byte[] sendMsg = new byte[4096];
-        byte[] receivedMsg = new byte[4096];
-
         static Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        static bool receiving = true;
+        static byte[] receivedMsg = new byte[4096];
+        
         public MainPage()
         {
             InitializeComponent();
-            client.Connect(remoteEP);
-            Ausgabe.Text = "Socket connected to " + client.RemoteEndPoint.ToString();
+            loopConnect();
+            Thread receiverThread = new Thread(()=> loopReceive());
+            receiverThread.Start();
         }
 
+        private void loopReceive()
+        {
+            while (true)
+            {
+                if(receiving == true)
+                {
+                    client.Receive(receivedMsg);
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        Ausgabe.Text = Ausgabe.Text + "\n" + Encoding.ASCII.GetString(receivedMsg);
+                    });
+                }
+            }
+        }
+
+        private static void loopConnect()
+        {
+            while (!client.Connected)
+            {
+                client.Connect(remoteEP);
+
+            }
+        }
         private void SendenButton_Clicked(object sender, EventArgs e)
         {
+            receiving = true;
 
-            sendMsg = Encoding.ASCII.GetBytes(Eingabe.Text);
-            
-            client.Send(sendMsg);
-            client.Receive(receivedMsg);
-            Ausgabe.Text = Encoding.ASCII.GetString(receivedMsg);
+            byte[] sendMsg = new byte[4096];
+            string message = "";
+            message = Eingabe.Text;
+
+            if (message != null)
+            {
+                sendMsg = Encoding.ASCII.GetBytes(Eingabe.Text);
+                if (client.Connected)
+                {
+                    client.Send(sendMsg);
+}
+                else
+                {
+                    client.Close();
+                }
+            }
+            receiving = false;
         }
     }
 }
